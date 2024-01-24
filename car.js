1559b2c7ed7a2c9df880d0af7049a924cb73c590
Niclas Juul Schæffer <Niclasschaeffer96@gmail.com>
2024-01-24 19:52:31 +0100

    Sensors

--- car.js
+++ car.js
@@ -11,11 +11,14 @@ class Car{
         this.friction=0.05;
         this.angle=0;
 
+        this.sensor=new Sensor(this);
         this.controls = new Controls();
     }
 
     update(){
         this.#move();
+        this.sensor.update();
+
     }
 
     #move(){
@@ -75,6 +78,8 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 
 }
