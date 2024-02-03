7f115d128f3d9f66c579e9adeac74bb71aaef2e1
Niclas Juul Schæffer <Niclasschaeffer96@gmail.com>
2024-02-03 21:48:44 +0100

    Finetuning

--- controls.js
+++ controls.js
@@ -1,52 +1,52 @@
 class Controls{
-    constructor(type){
+   constructor(type){
        this.forward=false;
        this.left=false;
        this.right=false;
        this.reverse=false;
- 
+
        switch(type){
-          case "KEYS":
-             this.#addKeyboardListeners();
-             break;
-          case "DUMMY":
-             this.forward=true;
-             break;
+           case "KEYS":
+               this.#addKeyboardListeners();
+               break;
+           case "DUMMY":
+               this.forward=true;
+               break;
        }
-    }
- 
-    #addKeyboardListeners(){
+   }
+
+   #addKeyboardListeners(){
        document.onkeydown=(event)=>{
-          switch(event.key){
-             case "ArrowLeft":
-                this.left=true;
-                break;
-             case "ArrowRight":
-                this.right=true;
-                break;
-             case "ArrowUp":
-                this.forward=true;
-                break;
-             case "ArrowDown":
-                this.reverse=true;
-                break;
-          }
+           switch(event.key){
+               case "ArrowLeft":
+                   this.left=true;
+                   break;
+               case "ArrowRight":
+                   this.right=true;
+                   break;
+               case "ArrowUp":
+                   this.forward=true;
+                   break;
+               case "ArrowDown":
+                   this.reverse=true;
+                   break;
+           }
        }
        document.onkeyup=(event)=>{
-          switch(event.key){
-             case "ArrowLeft":
-                this.left=false;
-                break;
-             case "ArrowRight":
-                this.right=false;
-                break;
-             case "ArrowUp":
-                this.forward=false;
-                break;
-             case "ArrowDown":
-                this.reverse=false;
-                break;
-          }
+           switch(event.key){
+               case "ArrowLeft":
+                   this.left=false;
+                   break;
+               case "ArrowRight":
+                   this.right=false;
+                   break;
+               case "ArrowUp":
+                   this.forward=false;
+                   break;
+               case "ArrowDown":
+                   this.reverse=false;
+                   break;
+           }
        }
-    }
- }
+   }
+}
